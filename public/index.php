54b572a9330f64858c610d3066602dba46b1685a
Barbara Fick <bkantzos@yahoo.com>
2019-07-26 18:13:45 -0600

    php printList function working

--- public/index.php
+++ public/index.php
@@ -1,35 +1,53 @@
 <!doctype html>
 
-<html lang="en">
+<html lang='en'>
   <head>
-    <title>GBI - <?php echo h($page_title); ?></title>
-    <meta charset="utf-8">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,900" rel="stylesheet">
-    <link rel="stylesheet" media="all" href="<?php echo url_for('style.css'); ?>" />
+    <title>Sample Problem</title>
+    <meta charset='utf-8'>
+    <link rel='stylesheet' media='all' href='styles.css'>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:200,400,700' rel='stylesheet'>
   </head>
   
+  <body>
+    <div class='brand'>
+      <h1><a href='ficktastic.com'>barbara fick</a></h1>
+    </div>
   
 <?php
 include 'db_connection.php';
 $conn = OpenCon();
-echo "Connected Successfully";
+echo 'Connected to the Database Successfully Yo';
 ?>
 
 <?php 
+
+function categoryTree($subItemOfID, $sub_mark){
+
     global $conn;
-    $query = $conn->query("SELECT * FROM categories");
+    $query = $conn->query("SELECT * FROM categories WHERE subItemOfID = $subItemOfID ORDER BY Item ASC");
 
     if($query->num_rows > 0){
          while($row = $query->fetch_assoc()){
-            echo $row['Item'];
             echo '<br />';
+            echo $sub_mark.$row['Item'];
+            categoryTree($row['itemID'], $sub_mark.'---');
         }
     }
+  }
 
 ?>
+
+
+<?php categoryTree(0, ''); ?>
+
 <?php
-echo "
+echo '
+<h1>ASSIGNMENT</h1>
 <p>
+Please provide a simple solution to the following problem in a language of your choice:
+</p>
+We want to be able to output a nested list of items &mdash; items in the list can be sub-items of other items in the list.  A simple database table would have the following fields:
+
 <ul>
 <li>Create Database Table</li>
 <li>Connect to Database</li>
@@ -44,13 +62,14 @@ echo "
 Initially, I wrote a solution in JavaScript 
 since that is the language I am most comfortable in at the moment.
 I created an object of objects and then realized that the 
-best data structure is a tree. I perhaps shouldn't admit that,
-since this php solution works.
+best data structure is a tree.
+
 </p>
 <p>
-Then I rewrote the function in php and created this page.
+Then I rewrote the function in php and created this page.  I am using 
 </p>
-";
+
+';
 ?>
 <?php 
 CloseCon($conn);
